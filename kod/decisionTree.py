ca2542f1a23a21320c3e4efaffcb89052b0c3eeb
Kacper Radzikowski <kacper.radzikowski2.stud@pw.edu.pl>
2024-01-21 17:02:29 +0100

    Algorithm is working

--- kod/decisionTree.py
+++ kod/decisionTree.py
@@ -4,13 +4,21 @@ from node import Node
 from collections import Counter
 
 class DecisionTree:
-    def __init__(self, min_sample_size, max_depth):
+    def __init__(self, min_sample_size=4, max_depth=100, gini_index=False):
         self.min_sample_size = min_sample_size
         self.max_depth = max_depth
+        self.gini_index = gini_index
         self.root = None
 
-    def fit(self):
-        self.root = self._crete_tree()
+    def fit(self, data, values):
+        self.root = self._create_tree(data, values)
+
+    def predict(self, X):
+        results = []
+        for x in X:
+            result = self._traverse_tree(x, self.root)
+            results.append(result)
+        return np.array(results)
 
     def _create_tree(self, data, values, depth=0):
         n_samples, n_attr = data.shape
@@ -25,6 +33,11 @@ class DecisionTree:
         best_attribute, best_criteria = self._decide_best_split(data, values, n_attr)
 
         # create child nodes of the tree
+        left_idxs, right_idxs = self._split(data[:, best_attribute], best_criteria)
+        left_child = self._create_tree(data[left_idxs, :], values[left_idxs], depth+1)
+        right_child = self._create_tree(data[right_idxs, :], values[right_idxs], depth+1)
+
+        return Node(best_attribute, best_criteria, left_child, right_child)
 
     def decide_leaf_value(self, y):
         counter = Counter(y)
@@ -36,13 +49,13 @@ class DecisionTree:
         split_atrtribute, split_criterium = None, None
 
         for i in range(n_attr):
-            X_column = [:, i]
+            X_column = X[:, i]
             possible_attr_values = np.unique(X_column)
             combinations_to_check = len(possible_attr_values) // 2
             possible_criteria = []
 
-            for c in range(1, combinations_to_check):
-                possible_criteria.append(combinations(possible_attr_values, c))
+            for c in (1, combinations_to_check + 1):
+                possible_criteria.extend(list(combinations(possible_attr_values, c)))
 
             for combination in possible_criteria:
                 # calculate information gain
@@ -56,4 +69,92 @@ class DecisionTree:
         return split_atrtribute, split_criterium
 
     def _information_gain(self, y, column, criterium):
-        pass
+        if self.gini_index:
+            # parent Gini Index
+            parent_gini = self._gini_index(y)
+
+            # create children
+            left_idxs, right_idxs = self._split(column, criterium)
+
+            if len(left_idxs) == 0 or len(right_idxs) == 0:
+                return 0
+
+            # calculate the weighted Gini Index of children
+            n = len(y)
+            n_left = len(left_idxs)
+            n_right = len(right_idxs)
+
+            gini_left = self._gini_index(y[left_idxs])
+            gini_right = self._gini_index(y[right_idxs])
+            child_gini = (n_left / n) * gini_left + (n_right / n) * gini_right
+
+            information_gain = parent_gini - child_gini
+
+        else:
+            # parent entropy
+            parent_entropy = self._entropy(y)
+
+            # create children
+            left_idxs, right_idxs = self._split(column, criterium)
+
+            if len(left_idxs) == 0 or len(right_idxs) == 0:
+                return 0
+
+            # calculate the weighted entropy of children
+            n = len(y)
+            n_left = len(left_idxs)
+            n_right = len(right_idxs)
+
+            entropy_left = self._entropy(y[left_idxs])
+            entropy_right = self._entropy(y[right_idxs])
+            child_entropy = (n_left / n) * entropy_left + (n_right / n) * entropy_right
+
+            information_gain = parent_entropy - child_entropy
+
+        return information_gain
+
+    def _gini_index(self, y):
+        gini_index = 1
+
+        supporting_histogram = np.bincount(y)
+        probababilities = supporting_histogram / len(y) # dividing every value in y by number of examples in dataset
+
+        for probability in probababilities:
+            if probability > 0:
+                to_sum = pow(probability, 2)
+                gini_index -= to_sum
+
+        return gini_index
+
+    def _entropy(self, y):
+        entropy = 0
+
+        supporting_histogram = np.bincount(y)
+        probababilities = supporting_histogram / len(y) # dividing every value in y by number of examples in dataset
+
+        for probability in probababilities:
+            if probability > 0:
+                to_sum = probability * np.log2(probability)
+                entropy -= to_sum
+
+        return entropy
+
+    def _split(self, X_column, criterium_to_split):
+        left_idxs = []
+        right_idxs = []
+        for idx in range(len(X_column)):
+            if X_column[idx] in criterium_to_split:
+                left_idxs.append(idx)
+            else:
+                right_idxs.append(idx)
+
+        return left_idxs, right_idxs
+
+    def _traverse_tree(self, x, node):
+        if node.isLeafNode():
+            return node.getValue()
+
+        if x[node.getFeature()] in node.getCriteria():
+            return self._traverse_tree(x, node.getLeftChild())
+        else:
+            return self._traverse_tree(x, node.getRightChild())
--- kod/main.py
+++ kod/main.py
@@ -1,33 +1,95 @@
 from loadData import parseDataToTrainDNA
 import numpy as np
 import classTrainDNA
+from decisionTree import DecisionTree
+from random import randint
 
 PATH_DTrain = "dane/spliceDTrainKIS.dat"
 PATH_ATrain = "dane/spliceATrainKIS.dat"
+PATH_ATrain_small = "dane/spliceATrainKIS_small.dat"
+
+def createPoolTeachAndVerify(data_array, teach_ratio = float):
+    data_array_size = len(data_array)
+    teach_array = []
+    teach_array_szie = int(data_array_size * teach_ratio)
+    verify_array = []
+
+    pos_data_elemets = []
+    neg_data_elemets = []
+
+    # count ratio of positives examples in data array
+    pos_counter = 0
+    for exmpl in data_array:
+        if(exmpl.getState() == 1):
+            pos_counter += 1
+
+    pos_ratio = pos_counter / len(data_array)
+
+    # separate data into positives and negatives array to split data correctly
+    for i in range(0, data_array_size):
+        if(data_array[i].getState() == 1):
+            pos_data_elemets.append(data_array[i])
+        else:
+            neg_data_elemets.append(data_array[i])
+
+    # add target amount of positives data into teach array
+    teach_pos_number = int(pos_ratio * teach_array_szie)
+    for i in range(0, teach_pos_number):
+        index = randint(0, len(pos_data_elemets)-1)
+        teach_array.append(pos_data_elemets[index])
+        del pos_data_elemets[index]
+
+    # add rest of positives examples to verify array
+    for element in pos_data_elemets:
+        verify_array.append(element)
+
+     # add target amount of positives data into teach array
+    teach_neg_number = int(teach_array_szie - teach_pos_number)
+    for i in range(0, teach_neg_number):
+        index = randint(0, len(neg_data_elemets)-1)
+        teach_array.append(neg_data_elemets[index])
+        del neg_data_elemets[index]
+
+    # add rest of positives examples to verify array
+    for element in neg_data_elemets:
+        verify_array.append(element)
+
+    return teach_array, verify_array
+
 
 def main():
 
     dtrain_array = parseDataToTrainDNA(PATH_DTrain)
     atrain_array = parseDataToTrainDNA(PATH_ATrain)
+    atrain_small_array = parseDataToTrainDNA(PATH_ATrain_small)
 
-    dtrain_attribute_val = [dtrain.getAttributes() for dtrain in dtrain_array]
-    atrain_attribute_val = [atrain.getAttributes() for atrain in atrain_array]
+    #create teach and verify pool by given data, ratio is teach size to whole array size
+    teach_array, verify_array = createPoolTeachAndVerify(atrain_array, 0.5)
 
-    possible_attribute_val_d = np.unique(dtrain_attribute_val).tolist()
-    possible_attribute_val_a = np.unique(atrain_attribute_val).tolist()
+    #prepare data to be passed to decision tree
+    verify_array_data = []
+    verify_array_values = []
+    for element in verify_array:
+        verify_array_values.append(element.getState())
+        verify_array_data.append(element.getAttributes())
 
-    #print first and last data train from Dtrain
-    dtrain_array[0].print()
-    dtrain_array[len(dtrain_array)-1].print()
+    teach_array_data = []
+    teach_array_values = []
+    for element in teach_array:
+        teach_array_values.append(element.getState())
+        teach_array_data.append(element.getAttributes())
 
-    #print first and last data train from Atrain
-    atrain_array[0].print()
-    atrain_array[len(atrain_array)-1].print()
+    classifier = DecisionTree(min_sample_size=1)
+    classifier.fit(np.array(teach_array_data), np.array(teach_array_values))
 
-    print("Possible attribute values for dtrain_array: " + str(possible_attribute_val_d))
-    print("Possible attribute values for atrain_array: " + str(possible_attribute_val_a))
+    predicitions = classifier.predict(verify_array_data)
+    acc = accuracy(verify_array_values, predicitions)
+    print(acc)
 
     pass
+
+def accuracy(y_test, y_pred):
+    return np.sum(y_test == y_pred) / len(y_test)
 
 if __name__ == "__main__":
     main()
